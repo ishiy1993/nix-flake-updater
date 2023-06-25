1f368c92740d4a7cd0b990a9c6699ea902f32004
ishiy <y.ishihara.1993@gmail.com>
2023-06-25 09:30:12 +0900

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
   description = "nix-flake-updater";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { inherit system; };
       in
       {
         devShells.default = pkgs.mkShell {
